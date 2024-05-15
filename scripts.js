3d6464333beeb044116c0692200cd065cbdeadc0
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-05-15 15:16:20 +0200

    called the initializeDocument function

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 let page = 1;
 let matches = books
 
-
+initializeDocument ()
 
 const initializeDocument = () => {
     const starting = document.createDocumentFragment()
